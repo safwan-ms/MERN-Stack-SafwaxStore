551c241893a20ccc6cd66cc7d7a5bd33fabdfc44
safwan-ms <safwan.anasms@gmail.com>
2025-01-31 20:17:27 +0530

    Redoing the login functionality better clarifation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -1,4 +1,100 @@
+import { useState } from "react";
+import {
+  AiOutlineHome,
+  AiOutlineShopping,
+  AiOutlineLogin,
+  AiOutlineUserAdd,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import "./Navigation.css";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router";
+import { useNavigate } from "react-router";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+  const toggleSidebar = () => setShowSidebar(!showSidebar);
+  return (
+    <div
+      style={{ zIndex: 9999 }}
+      className={`${
+        showSidebar ? "hidden" : "flex"
+      } hidden xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-2 xl:p-3 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+      id="navigation-container"
+    >
+      <div className="flex flex-col justify-center space-y-4">
+        {/* Home */}
+        <Link
+          to="/"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem] text-white">
+            HOME
+          </span>
+        </Link>
+
+        {/* Shopping */}
+        <Link
+          to="/shop"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem] text-white">
+            SHOP
+          </span>
+        </Link>
+
+        {/* Cart */}
+        <Link
+          to="/cart"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem] text-white">
+            CART
+          </span>
+        </Link>
+
+        {/* Favorite */}
+        <Link
+          to="/favorite"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <FaHeart className="mr-2 mt-[3rem]" size={26} />
+          <span className="hidden nav-item-name mt-[3rem] text-white">
+            FAVORITE
+          </span>
+        </Link>
+      </div>
+
+      <ul>
+        <li>
+          <Link
+            to="/login"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineLogin className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem] text-white">
+              Login
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/register"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26} />
+            <span className="hidden nav-item-name mt-[3rem] text-white">
+              Register
+            </span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Navigation;
