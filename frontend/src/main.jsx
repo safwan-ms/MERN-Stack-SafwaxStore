0345a9b9a4f01e8f6359e0afd9b948202db696aa
safwan-ms <safwan.anasms@gmail.com>
2025-01-31 19:08:40 +0530

    refactor: refining the user management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,10 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
-import { RouterProvider, Route, createRoutesFromElements } from "react-router";
-import { createBrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store.js";
 
-//Auth
-import Login from "./pages/Auth/Login.jsx";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/login" element={<Login />} />
-    </Route>
-  )
-);
-
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
