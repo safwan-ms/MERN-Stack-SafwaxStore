c68b2c0628ed25f49ed34b17bd2a73b0ab899473
safwan-ms <safwan.anasms@gmail.com>
2025-02-10 22:20:52 +0530

    feat: implement of updateProduct function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,8 +27,42 @@ const addProduct = asyncHandler(async (req, res) => {
     res.status(201).json(product);
   } catch (error) {
     console.log("Error occurred in addProduct ", error);
-    res.status(400).json(error.message);
+    res.status(400).json("Failed to save product", error.message);
   }
 });
 
-export { addProduct };
+const updateProductDetails = asyncHandler(async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, brand } = req.fields;
+
+    switch (true) {
+      case !name:
+        return res.status(400).json({ error: "Name is required!" });
+      case !description:
+        return res.status(400).json({ error: "Description is required!" });
+      case !price:
+        return res.status(400).json({ error: "Price is required!" });
+      case !category:
+        return res.status(400).json({ error: "Category is required!" });
+      case !quantity:
+        return res.status(400).json({ error: "Quantity is required!" });
+      case !brand:
+        return res.status(400).json({ error: "Brand is required!" });
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { ...req.fields },
+      { new: true }
+    );
+
+    if (!product) return res.status(404).json({ error: "Product not found!" });
+
+    await product.save();
+    res.status(202).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json("Failed to update product", error.message);
+  }
+});
+export { addProduct, updateProductDetails };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,8 +5,16 @@ import {
   authorizedAdmin,
 } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
-import { addProduct } from "../controllers/productController.js";
+import {
+  addProduct,
+  updateProductDetails,
+} from "../controllers/productController.js";
+
 const router = express.Router();
+
 router.route("/").post(authenticate, authorizedAdmin, formidable(), addProduct);
+router
+  .route("/:id")
+  .put(authenticate, authorizedAdmin, formidable(), updateProductDetails);
 
 export default router;
