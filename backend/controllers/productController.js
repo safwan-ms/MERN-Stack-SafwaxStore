63494ecef9902b15d66f3b4440f46ee0906f0475
safwan-ms <safwan.anasms@gmail.com>
2025-02-10 21:32:11 +0530

    Initiate addProduct functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,12 +4,27 @@ import Product from "../models/productModel.js";
 const addProduct = asyncHandler(async (req, res) => {
   try {
     const { name, description, price, category, quantity, brand } = req.fields;
-    console.log(name);
-    console.log(description);
-    console.log(price);
-    console.log(category);
-    console.log(quantity);
-    console.log(brand);
+
+    //Validation
+    switch (true) {
+      case !name:
+        return res.status(400).json({ error: "Name is required!" });
+      case !description:
+        return res.status(400).json({ error: "Description is required!" });
+      case !price:
+        return res.status(400).json({ error: "Price is required!" });
+      case !category:
+        return res.status(400).json({ error: "Category is required!" });
+      case !quantity:
+        return res.status(400).json({ error: "Quantity is required!" });
+      case !brand:
+        return res.status(400).json({ error: "Brand is required!" });
+    }
+
+    const product = Product({ ...req.fields });
+    await product.save();
+
+    res.status(201).json(product);
   } catch (error) {
     console.log("Error occurred in addProduct ", error);
     res.status(400).json(error.message);
