eaeab442de38c85fdcd1533b4cf3b8231a421dd4
safwan-ms <safwan.anasms@gmail.com>
2025-02-13 15:14:58 +0530

    feat: Implement image upload and product management logic

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 //packages
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,6 +9,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -23,5 +24,9 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes); //User Routes
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`Server is listening to port ${port}`));
