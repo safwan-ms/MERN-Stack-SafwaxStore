8ba2a25437e20ac2d7fbc6ee000c3f485c3da47d
safwan-ms <safwan.anasms@gmail.com>
2025-02-18 23:47:41 +0530

    chore: Made setup for Special Products

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,12 +3,34 @@ import { useGetProductsQuery } from "./redux/api/productApiSlice";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
 import Message from "./components/Message.jsx";
-import Error from "./components/Error.jsx";
 
 const Home = () => {
   const { keyword } = useParams();
   const { data, isLoading, isError } = useGetProductsQuery({ keyword });
 
-  return <>{!keyword ? <Header /> : null}</>;
+  return (
+    <>
+      {!keyword ? <Header /> : null}
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message variant="danger">
+          {isError?.data.message || isError.message}
+        </Message>
+      ) : (
+        <>
+          <div className="flex justify-around items-center mt-10 py-10">
+            <h1 className="text-2xl">Special Products</h1>
+            <Link
+              className="bg-pink-500 px-3 md:px-4 py-2 rounded-full"
+              to={"/shop"}
+            >
+              Shop
+            </Link>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 export default Home;
--- frontend/src/pages/Products/SmallProduct.jsx
+++ frontend/src/pages/Products/SmallProduct.jsx
@@ -2,25 +2,27 @@ import { Link } from "react-router-dom";
 
 const SmallProduct = ({ product }) => {
   return (
-    <div className="w-[150px] md:w-[200px] h-[200px] md:h-[300px] m-[0.5rem]  flex flex-col justify-between">
-      <div className="relative">
-        <img
-          src={product.image}
-          alt={product.name}
-          className="h-[150px] md:h-[200px] w-[150px] md:w-[200px] object-cover rounded"
-        />
-      </div>
-      <div className="p-1 flex-1 flex flex-col justify-between">
-        <Link to={`/product/${product._id}`}>
-          <div className="flex justify-between items-center">
-            <h2 className="font-bold text-sm w-[170px]">
-              {product.name.substring(0, 15)}...
-            </h2>
-            <span className="bg-pink-100 text-xs md:text-sm font-medium px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300">
-              ₹{new Intl.NumberFormat("en-IN").format(product.price)}
-            </span>
-          </div>
-        </Link>
+    <div className="flex justify-center items-center">
+      <div className="w-[130px] sm:w-[200px] md:w-[300px] m-[0.5rem] sm:m-[2rem] flex flex-col justify-between">
+        <div className="relative">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="h-[130px] sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[200px] w-[130px] object-cover rounded"
+          />
+        </div>
+        <div className="p-1 flex-1 flex flex-col justify-between">
+          <Link to={`/product/${product._id}`}>
+            <div className="flex justify-between items-center">
+              <h2 className="font-bold text-sm w-[170px]">
+                {product.name.substring(0, 30)}...
+              </h2>
+              <span className="bg-pink-100 text-xs md:text-sm font-medium px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300">
+                ₹{new Intl.NumberFormat("en-IN").format(product.price)}
+              </span>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
