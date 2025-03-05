5351ada5f27eb0db2401f4f3109d03d7976e7960
safwan-ms <safwan.anasms@gmail.com>
2025-03-05 20:56:53 +0530

    feat: Add responsive Filteration input UI

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -15,7 +15,10 @@ const Shop = () => {
   );
 
   const categoriesQuery = useFetchCategoriesQuery();
+
+  const [showFilters, setShowFilters] = useState(false);
   const [priceFilter, setPriceFilter] = useState("");
+
   const filteredProductsQuery = useGetFilterProductsQuery({
     checked,
     radio,
@@ -77,6 +80,50 @@ const Shop = () => {
     setPriceFilter(e.target.value);
   };
 
-  return <div>Shop</div>;
+  return (
+    <div className="container mx-auto mt-10 md:mt-16">
+      <div className="flex flex-col md:flex-row">
+        {/* Toggle button for small screens */}
+        <button
+          onClick={() => setShowFilters(!showFilters)}
+          className="md:hidden bg-black text-white px-4 py-2 rounded-full mx-3 my-2"
+        >
+          {showFilters ? "Hide Filters" : "Show Filters"}
+        </button>
+
+        {/* Filter section */}
+        <div
+          className={`bg-[#151515] p-3 mt-2 mb-2 ${
+            showFilters ? "block" : "hidden"
+          } md:block w-full md:w-auto max-w-sm`}
+        >
+          <h2 className="h4 text-center py-2 bg-black rounded-full mb-2">
+            Filter by Categories
+          </h2>
+          <div className="p-5 w-full md:w-[15rem]">
+            {categories.map((c) => (
+              <div key={c._id} className="mb-2">
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    id={`checkbox-${c.id}`}
+                    onChange={(e) => handleCheck(e.target.checked, c.id)}
+                    className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    htmlFor={`checkbox-${c.id}`}
+                    className="ml-2 text-sm font-medium text-white dark:text-gray-300"
+                  >
+                    {c.name}
+                  </label>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default Shop;
