c6607f0219a8173e349a15d055e131738c3ddef3
safwan-ms <safwan.anasms@gmail.com>
2025-03-07 15:11:12 +0530

    feat: Creating Advance Shopping Experience: Product Filtering and Cart Integration

---
+++ frontend/src/pages/Products/ProductCard.jsx
@@ -0,0 +1,79 @@
+import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice.js";
+import { toast } from "react-toastify";
+import HeartIcon from "./HeartIcon.jsx";
+
+const ProductCard = ({ p }) => {
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (product, qty) => {
+    dispatch(addToCart({ ...product, qty }));
+    toast.success("Item added successfully");
+  };
+  return (
+    <div className="relative bg-[#1d1b1b] rounded-lg shadow ">
+      <section className="relative">
+        <Link to={`/product/${p._id}`}>
+          <span className="absolute bottom-3 right-3 bg-pink-100 text-pink-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300">
+            {p?.brand}
+          </span>
+          <img
+            className="cursor-pointer w-full h-[220px] object-cover"
+            src={p.image}
+            alt={p.name}
+          />
+        </Link>
+        <HeartIcon product={p} />
+      </section>
+
+      <div className="p-5">
+        <div className="flex justify-between ">
+          <h5 className="mb-2  text-xs xl:text-sm text-white dark:text-white">
+            {p?.name.substring(0, 30)}...
+          </h5>
+
+          <p className="font-semibold text-xs text-pink-500">
+            {Intl.NumberFormat("en-IN").format(p?.price)}
+          </p>
+        </div>
+
+        <p className=" font-normal mb-3 text-[12px] text-[#CFCFCF]">
+          {p?.description.substring(0, 30)}...
+        </p>
+
+        <section className="flex justify-between items-center">
+          <Link
+            to={`/product/${p._id}`}
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-700 rounded-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+          >
+            Read More
+            <svg
+              className="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </Link>
+          <button
+            className="p-2 rounded-full cursor-pointer hover:bg-[#363434] focus:bg-[#363434]"
+            onClick={() => addToCartHandler(p, 1)}
+          >
+            <AiOutlineShoppingCart size={25} />
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+};
+export default ProductCard;
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -7,6 +7,8 @@ import {
   setChecked,
 } from "../redux/features/shop/shopSlice.js";
 import { useFetchCategoriesQuery } from "../redux/api/categoryApiSlice.js";
+import ProductCard from "./Products/ProductCard.jsx";
+import Loader from "../components/Loader.jsx";
 
 const Shop = () => {
   const dispatch = useDispatch();
@@ -15,8 +17,8 @@ const Shop = () => {
   );
 
   const categoriesQuery = useFetchCategoriesQuery();
-  const [showFilters, setShowFilters] = useState(false);
   const [priceFilter, setPriceFilter] = useState("");
+  const [showFilters, setShowFilters] = useState(false);
 
   const filteredProductsQuery = useGetFilterProductsQuery({
     checked,
@@ -24,24 +26,22 @@ const Shop = () => {
   });
 
   useEffect(() => {
-    if (categoriesQuery.data && !categoriesQuery.isLoading) {
+    if (!categoriesQuery.isLoading) {
       dispatch(setCategories(categoriesQuery.data));
     }
   }, [categoriesQuery.data, dispatch]);
 
   useEffect(() => {
-    if (
-      filteredProductsQuery.data?.length > 0 &&
-      checked.length === 0 &&
-      radio.length === 0
-    ) {
-      const filteredProducts = filteredProductsQuery.data.filter(
-        (product) =>
-          product.price.toString().includes(priceFilter) ||
-          product.price === parseInt(priceFilter, 10)
-      );
+    if (!checked.length || !radio.length) {
+      if (!filteredProductsQuery.isLoading) {
+        const filteredProducts = filteredProductsQuery.data.filter(
+          (product) =>
+            product.price.toString().includes(priceFilter) ||
+            product.price === parseInt(priceFilter, 10)
+        );
 
-      dispatch(setProducts(filteredProducts));
+        dispatch(setProducts(filteredProducts));
+      }
     }
   }, [checked, radio, filteredProductsQuery.data, dispatch, priceFilter]);
 
@@ -60,38 +60,38 @@ const Shop = () => {
   };
 
   const uniqueBrands = [
-    "All Brands",
-    ...new Set(
-      filteredProductsQuery.data
-        ?.map((product) => product.brand)
-        .filter((brand) => brand !== undefined)
+    ...Array.from(
+      new Set(
+        filteredProductsQuery.data
+          ?.map((product) => product.brand)
+          .filter((brand) => brand !== undefined)
+      )
     ),
   ];
 
   const handlePriceChange = (e) => {
-    // Update the price filter state when the user types in the input filed
     setPriceFilter(e.target.value);
   };
 
   return (
-    <div className="container mx-5 mt-10 md:mt-16">
-      <div className="flex flex-col md:flex-row">
+    <div className="container mx-auto px-5 mt-10 md:mt-16">
+      <div className="flex flex-col md:flex-row gap-4">
         <button
           onClick={() => setShowFilters(!showFilters)}
-          className="md:hidden bg-black text-white px-4 py-2 rounded-full mx-3 my-2"
+          className="md:hidden bg-black text-white px-4 py-2 rounded-full"
         >
           {showFilters ? "Hide Filters" : "Show Filters"}
         </button>
 
         <div
-          className={`bg-[#151515] p-3 mt-2 mb-2 ${
+          className={`bg-[#151515] p-3 rounded-lg shadow-md ${
             showFilters ? "block" : "hidden"
-          } md:block w-full md:w-auto max-w-sm`}
+          } md:block w-full md:w-auto max-w-[90vw] md:max-w-xs`}
         >
           <h2 className="h4 text-center py-2 bg-black rounded-full mb-2">
             Filter by Categories
           </h2>
-          <div className="p-5 w-full md:w-[15rem]">
+          <div className="p-5">
             {categories.slice(1).map((c) => (
               <div key={c._id} className="mb-2">
                 <div className="flex items-center">
@@ -99,7 +99,7 @@ const Shop = () => {
                     type="checkbox"
                     id={`checkbox-${c._id}`}
                     onChange={(e) => handleCheck(e.target.checked, c._id)}
-                    className="w-4 checked:bg-pink-600 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
+                    className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500"
                   />
                   <label
                     htmlFor={`checkbox-${c._id}`}
@@ -115,40 +115,33 @@ const Shop = () => {
           <h2 className="text-center py-2 bg-black rounded-full mb-2">
             Filter By Brands
           </h2>
-          <div className="p-5">
+          <div className="p-5 flex flex-wrap gap-2">
             {uniqueBrands.map((brand) => (
-              <>
-                <div className="flex items-center mr-4 mb-5">
-                  <input
-                    type="radio"
-                    id={brand}
-                    name="brand"
-                    onChange={() => handleBrandClick(brand)}
-                    className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 focus:ring-pink-500 checked:bg-pink-600 checked:border-pink-600"
-                  />
-
-                  <label
-                    htmlFor={brand}
-                    className="ml-2 text-sm font-medium text-white dark:text-gray-300"
-                  >
-                    {brand}
-                  </label>
-                </div>
-              </>
+              <div key={brand} className="flex items-center">
+                <input
+                  type="radio"
+                  id={brand}
+                  name="brand"
+                  onChange={() => handleBrandClick(brand)}
+                  className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 focus:ring-pink-500"
+                />
+                <label htmlFor={brand} className="ml-2 text-sm text-white">
+                  {brand}
+                </label>
+              </div>
             ))}
           </div>
 
           <h2 className="text-center py-2 bg-black rounded-full mb-2">
             Filter By Price
           </h2>
-
-          <div className="p-5 w-[15rem]">
+          <div className="p-5">
             <input
               type="text"
               placeholder="Enter Price"
               value={priceFilter}
               onChange={handlePriceChange}
-              className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg focus:outline-none focus:ring focus:ring-pink-500 focus:border-pink-600"
+              className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg focus:ring-pink-500"
             />
           </div>
 
@@ -162,8 +155,19 @@ const Shop = () => {
           </div>
         </div>
 
-        <div className="p-3">
+        <div className="flex-grow p-3">
           <h2 className="h4 text-center mb-2">{products?.length} Products</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+            {products.length === 0 ? (
+              <Loader />
+            ) : (
+              products?.map((p) => (
+                <div key={p._id} className="p-3">
+                  <ProductCard p={p} />
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
