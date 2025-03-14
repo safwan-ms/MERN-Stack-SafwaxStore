efa5ac2621a87798237fa7ae9433691f0c71d28b
safwan-ms <safwan.anasms@gmail.com>
2025-03-14 22:14:09 +0530

    feat: Add order-related queries and mutations in API slice

--- frontend/src/redux/api/orderApiSlice.js
+++ frontend/src/redux/api/orderApiSlice.js
@@ -16,5 +16,45 @@ export const orderApiSlice = apiSlice.injectEndpoints({
         url: `${ORDERS_URL}/${id}`,
       }),
     }),
+
+    payOrder: builder.mutation({
+      query: ({ orderId, details }) => ({
+        url: `${ORDERS_URL}/${orderId}/pay`,
+        method: "PUT",
+        body: details,
+      }),
+    }),
+
+    getPaypalClientId: builder.query({
+      query: () => ({
+        url: PAYPAL_URL,
+      }),
+    }),
+
+    getMyOrders: builder.query({
+      query: () => ({
+        url: `${ORDERS_URL}/mine`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    deliverOrder: builder.mutation({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}/deliver`,
+        method: "PUT",
+      }),
+    }),
+
+    getTotalOrders: builder.query({
+      query: () => `${ORDERS_URL}/total-orders`,
+    }),
+
+    getTotalSales: builder.query({
+      query: () => `${ORDERS_URL}/total-sales`,
+    }),
+
+    getTotalSalesByDate: builder.query({
+      query: () => `${ORDERS_URL}/total-sales-by-date`,
+    }),
   }),
 });
