3323dad2a1ee9f2ae384dc411716613a1f0c5d1b
SafwaX-WebDeV <safwan.anasms@gmail.com>
2025-04-18 23:11:22 +0530

    Fix: Fix image upload for hosted backend by switching to multer memoryStorage

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,21 +1,12 @@
-import path from "path";
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const extname = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${Date.now()}${extname}`);
-  },
-});
+const storage = multer.memoryStorage(); // use memoryStorage for cloud uploads
 
 const fileFilter = (req, file, cb) => {
   const filetypes = /jpe?g|png|webp/;
   const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
 
-  const extname = path.extname(file.originalname);
+  const extname = file.originalname;
   const mimetype = file.mimetype;
 
   if (filetypes.test(extname) && mimetypes.test(mimetype)) {
